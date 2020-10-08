3d527709a98e20ef733a4570d9eb918a45b19c58
MalbonaGenio <pvalverdemolina@gmail.com>
2020-10-08 01:30:55 +0000

    When submitting now new array gets reset and rendered

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
 const emojiContainer = document.getElementById("emoji-container")
-
-for (let i = 0; i < myEmojis.length; i++) {
-    const emoji = document.createElement('span')
-    emoji.textContent = myEmojis[i]
-    emojiContainer.append(emoji)
-}
-
 const pushBtn = document.getElementById("push-btn")
 pushBtn.addEventListener("click", inputButton)
+
+//renders emojis in the emojiContainer
+function renderEmoji(){
+    for (let i = 0; i < myEmojis.length; i++) {
+        const emoji = document.createElement('span')
+        emoji.textContent = myEmojis[i]
+        emojiContainer.append(emoji)
+    }
+}
+renderEmoji() //first call for rendering emoji on emojiContainer
 
 function inputButton(){
     const emojiInput = document.getElementById("emoji-input")
 
-//empty string evaluates to false so instead of: if (emojiInput.value !== '') we can:
+    //empty string evaluates to false so instead of: if (emojiInput.value !== '') we can:
     if (emojiInput.value)  {
         myEmojis.push(emojiInput.value)
-        emojiInput.value = ''
-
+        emojiInput.value = '' //resets input box when submited.
+        emojiContainer.textContent = '' // resets emojiContainer
+        renderEmoji()
     }
    
 }
