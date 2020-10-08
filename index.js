146813622b689b408c9b47001b2a661764459af5
MalbonaGenio <pvalverdemolina@gmail.com>
2020-10-08 01:59:07 +0000

    Now we can put emojis at the begining using unshift. Also some code reorganization.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
-const emojiContainer = document.getElementById("emoji-container")
-const pushBtn = document.getElementById("push-btn")
-pushBtn.addEventListener("click", inputButton)
 
 //renders emojis in the emojiContainer
 function renderEmoji(){
+    const emojiContainer = document.getElementById("emoji-container")
+    emojiContainer.textContent = ''
+    
     for (let i = 0; i < myEmojis.length; i++) {
         const emoji = document.createElement('span')
         emoji.textContent = myEmojis[i]
@@ -13,15 +13,26 @@ function renderEmoji(){
 }
 renderEmoji() //first call for rendering emoji on emojiContainer
 
-function inputButton(){
-    const emojiInput = document.getElementById("emoji-input")
+const emojiInput = document.getElementById("emoji-input")
+const pushBtn = document.getElementById("push-btn")
+pushBtn.addEventListener("click", inputButtonEnd)
 
+function inputButtonEnd(){
     //empty string evaluates to false so instead of: if (emojiInput.value !== '') we can:
     if (emojiInput.value)  {
         myEmojis.push(emojiInput.value)
-        emojiInput.value = '' //resets input box when submited.
-        emojiContainer.textContent = '' // resets emojiContainer
+        emojiInput.value = '' 
         renderEmoji()
     }
-   
+}
+
+const unshiftBtn = document.getElementById("unshift-btn")
+unshiftBtn.addEventListener("click", inputButtonBeginning)
+
+function inputButtonBeginning(){
+    if (emojiInput.value)  {
+        myEmojis.unshift(emojiInput.value) 
+        emojiInput.value = '' 
+        renderEmoji()
+    }
 }
